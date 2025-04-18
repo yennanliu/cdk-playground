10914257d98587f57515bc08b47c2937cee34058
ynj12 <777@uber.com>
2025-04-18 14:30:23 +0800

    clean

--- crudl-api-stack/bin/crudl-api-stack.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { CrudlApiStackStack } from '../lib/crudl-api-stack-stack';
-
-const app = new cdk.App();
-new CrudlApiStackStack(app, 'CrudlApiStackStack');
