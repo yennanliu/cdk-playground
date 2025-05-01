03e1601a0ce72e87bc7ec8804925cd2bb7fd103c
yennanliu <f339339@gmail.com>
2025-05-01 20:01:22 +0800

    remove gitlab-deployment stack

--- gitlab-deployment/bin/gitlab-deployment.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { GitlabDeploymentStack } from '../lib/gitlab-deployment-stack';
-
-const app = new cdk.App();
-new GitlabDeploymentStack(app, 'GitlabDeploymentStack');
