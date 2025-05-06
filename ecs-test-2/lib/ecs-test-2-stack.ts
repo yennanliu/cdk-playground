29039b11cd88211ad2efff014705fd524c1f27a0
yennanliu <f339339@gmail.com>
2025-05-06 20:13:53 +0800

    update ecs-test-2-stack.ts stack

--- ecs-test-2/lib/ecs-test-2-stack.ts
+++ ecs-test-2/lib/ecs-test-2-stack.ts
@@ -1,19 +1,39 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
 
-export class EcsTest2Stack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class EcsTest2Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'EcsTest2Queue', {
-      visibilityTimeout: Duration.seconds(300)
+    // 1. Create a VPC with default settings
+    const vpc = new ec2.Vpc(this, "MyVpc", { maxAzs: 2 });
+
+    // 2. Create an ECS Cluster in the VPC
+    const cluster = new ecs.Cluster(this, "MyCluster", {
+      vpc: vpc,
     });
 
-    const topic = new sns.Topic(this, 'EcsTest2Topic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // 3. Create a Fargate Service with a Load Balancer
+    new ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      "MyFargateService",
+      {
+        cluster,
+        cpu: 256,
+        memoryLimitMiB: 512,
+        desiredCount: 1,
+        publicLoadBalancer: true,
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromRegistry("nginx"), // You can replace this with your image
+        },
+      }
+    );
   }
 }
