2cc2a594768c1fa49c17cd5ef39ded51202ad789
yennanliu <f339339@gmail.com>
2025-05-06 20:48:06 +0800

    implement ecs-spring-petclinic-rest stack

--- ecs-spring-petclinic-rest/lib/ecs-spring-petclinic-rest-stack.ts
+++ ecs-spring-petclinic-rest/lib/ecs-spring-petclinic-rest-stack.ts
@@ -1,19 +1,142 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { Stack, StackProps, Duration, RemovalPolicy } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  Vpc,
+  SubnetType,
+  SecurityGroup,
+  Peer,
+  Port,
+} from "aws-cdk-lib/aws-ec2";
+import {
+  Cluster,
+  ContainerImage,
+  FargateService,
+  FargateTaskDefinition,
+  LogDrivers,
+  Protocol,
+} from "aws-cdk-lib/aws-ecs";
+import {
+  ApplicationLoadBalancer,
+  ApplicationProtocol,
+  ApplicationTargetGroup,
+  ListenerAction,
+  TargetType,
+} from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import {
+  DatabaseInstance,
+  DatabaseInstanceEngine,
+  PostgresEngineVersion,
+  Credentials,
+} from "aws-cdk-lib/aws-rds";
+import { InstanceType, InstanceClass, InstanceSize } from "aws-cdk-lib/aws-ec2";
+import { SecretValue } from "aws-cdk-lib";
 
 export class EcsSpringPetclinicRestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'EcsSpringPetclinicRestQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // 1. VPC
+    const vpc = new Vpc(this, "AppVpc", {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          name: "public",
+          subnetType: SubnetType.PUBLIC,
+        },
+        {
+          name: "private",
+          subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+        },
+      ],
     });
 
-    const topic = new sns.Topic(this, 'EcsSpringPetclinicRestTopic');
+    // 2. RDS (PostgreSQL)
+    const dbSecurityGroup = new SecurityGroup(this, "DbSG", {
+      vpc,
+      description: "Allow ECS to access RDS",
+      allowAllOutbound: true,
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const db = new DatabaseInstance(this, "JavaAppDb", {
+      engine: DatabaseInstanceEngine.postgres({
+        version: PostgresEngineVersion.VER_15,
+      }),
+      vpc,
+      vpcSubnets: {
+        subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+      },
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+      credentials: Credentials.fromGeneratedSecret("postgres"),
+      allocatedStorage: 20,
+      maxAllocatedStorage: 100,
+      securityGroups: [dbSecurityGroup],
+      databaseName: "javaapp",
+      removalPolicy: RemovalPolicy.DESTROY, // NOT FOR PRODUCTION
+      publiclyAccessible: false,
+    });
+
+    // 3. ECS Cluster
+    const cluster = new Cluster(this, "AppCluster", { vpc });
+
+    // 4. Fargate Task
+    const taskDef = new FargateTaskDefinition(this, "JavaAppTask", {
+      memoryLimitMiB: 1024,
+      cpu: 512,
+    });
+
+    taskDef.addContainer("AppContainer", {
+      image: ContainerImage.fromRegistry("openjdk:17"), // Replace with your app image
+      logging: LogDrivers.awsLogs({ streamPrefix: "App" }),
+      environment: {
+        DB_HOST: db.dbInstanceEndpointAddress,
+        DB_PORT: db.dbInstanceEndpointPort,
+        DB_USER: "postgres",
+        DB_NAME: "javaapp",
+        // Use Secrets Manager in real app for password
+      },
+    });
+
+    // 5. ECS Service + SG
+    const appSG = new SecurityGroup(this, "AppSG", {
+      vpc,
+      description: "App access to DB",
+      allowAllOutbound: true,
+    });
+
+    // Allow app SG to connect to DB SG
+    dbSecurityGroup.addIngressRule(
+      appSG,
+      Port.tcp(5432),
+      "App access to Postgres"
+    );
+
+    const service = new FargateService(this, "JavaAppService", {
+      cluster,
+      taskDefinition: taskDef,
+      assignPublicIp: true,
+      securityGroups: [appSG],
+    });
+
+    // 6. Load Balancer
+    const lb = new ApplicationLoadBalancer(this, "AppLB", {
+      vpc,
+      internetConnected: true,
+      loadBalancerName: "JavaAppLB",
+      vpcSubnets: { subnetType: SubnetType.PUBLIC },
+    });
+
+    const listener = lb.addListener("AppListener", {
+      port: 80,
+      open: true,
+    });
+
+    listener.addTargets("AppTargets", {
+      port: 80,
+      targets: [service],
+      healthCheck: {
+        path: "/",
+        interval: Duration.seconds(60),
+      },
+    });
   }
 }
