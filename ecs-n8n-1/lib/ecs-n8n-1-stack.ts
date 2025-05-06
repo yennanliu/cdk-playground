c6d292b6895003e465dabde02440f8c4e8a8cee8
yennanliu <f339339@gmail.com>
2025-05-06 20:32:04 +0800

    implement ecs-n8n-1-stack

--- ecs-n8n-1/lib/ecs-n8n-1-stack.ts
+++ ecs-n8n-1/lib/ecs-n8n-1-stack.ts
@@ -1,19 +1,40 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
 
-export class EcsN8N1Stack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class EcsN8N1Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'EcsN8N1Queue', {
-      visibilityTimeout: Duration.seconds(300)
+    // 1. VPC (with default 2 AZs)
+    const vpc = new ec2.Vpc(this, "N8nVpc", {
+      maxAzs: 2,
     });
 
-    const topic = new sns.Topic(this, 'EcsN8N1Topic');
+    // 2. ECS Cluster
+    const cluster = new ecs.Cluster(this, "N8nCluster", {
+      vpc,
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // 3. Fargate Service with ALB
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "N8nService", {
+      cluster,
+      cpu: 512,
+      memoryLimitMiB: 1024,
+      desiredCount: 1,
+      publicLoadBalancer: true,
+      taskImageOptions: {
+        containerPort: 5678,
+        image: ecs.ContainerImage.fromRegistry("n8nio/n8n:latest"),
+        environment: {
+          // Optional: Add env vars for credentials, webhook base, etc.
+          N8N_BASIC_AUTH_ACTIVE: "true",
+          N8N_BASIC_AUTH_USER: "admin",
+          N8N_BASIC_AUTH_PASSWORD: "admin",
+        },
+      },
+    });
   }
 }
