8b71b88e5f0710dce454f8bb18b1f92eac203eb9
yennanliu <f339339@gmail.com>
2025-05-06 20:20:26 +0800

    update ecs-test-1-stack.ts stack

--- ecs-test-1/lib/ecs-test-1-stack.ts
+++ ecs-test-1/lib/ecs-test-1-stack.ts
@@ -1,19 +1,34 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
 import { Construct } from 'constructs';
 
 export class EcsTest1Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'EcsTest1Queue', {
-      visibilityTimeout: Duration.seconds(300)
+    // Create a VPC
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 2,
+      natGateways: 1
     });
 
-    const topic = new sns.Topic(this, 'EcsTest1Topic');
+    // Create an ECS cluster
+    const cluster = new ecs.Cluster(this, 'MyCluster', {
+      vpc: vpc
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // Create a Fargate service with a load-balanced web server
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'MyFargateService', {
+      cluster: cluster,
+      cpu: 256,
+      memoryLimitMiB: 512,
+      desiredCount: 1,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample')
+      },
+      publicLoadBalancer: true
+    });
   }
 }
