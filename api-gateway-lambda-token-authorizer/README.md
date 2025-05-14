780018c95739244ffee254b872ca76e72a1d9e97
yenj12 <yenj12@uber.com>
2025-05-14 17:26:43 +0800

    add lambda, implement stack api-gateway-lambda-token-authorizer

--- api-gateway-lambda-token-authorizer/README.md
+++ api-gateway-lambda-token-authorizer/README.md
@@ -13,3 +13,8 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `cdk deploy`      deploy this stack to your default AWS account/region
 * `cdk diff`        compare deployed stack with current state
 * `cdk synth`       emits the synthesized CloudFormation template
+
+
+## Ref
+
+- https://github.com/aws-samples/aws-cdk-examples/tree/main/typescript/api-gateway-lambda-token-authorizer
--- api-gateway-lambda-token-authorizer/bin/api-gateway-lambda-token-authorizer.ts
+++ api-gateway-lambda-token-authorizer/bin/api-gateway-lambda-token-authorizer.ts
@@ -3,4 +3,10 @@ import * as cdk from 'aws-cdk-lib';
 import { ApiGatewayLambdaTokenAuthorizerStack } from '../lib/api-gateway-lambda-token-authorizer-stack';
 
 const app = new cdk.App();
-new ApiGatewayLambdaTokenAuthorizerStack(app, 'ApiGatewayLambdaTokenAuthorizerStack');
+// new ApiGatewayLambdaTokenAuthorizerStack(app, 'ApiGatewayLambdaTokenAuthorizerStack');
+new ApiGatewayLambdaTokenAuthorizerStack(app, 'gateway-lambda-auth-stack', {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION
+    }
+});
