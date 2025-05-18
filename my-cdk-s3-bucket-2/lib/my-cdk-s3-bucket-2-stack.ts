444471db1becd68636a05814ba233ba1dc15d9f4
yennanliu <f339339@gmail.com>
2025-05-18 09:36:58 +0800

    my-cdk-s3-bucket-2 update bucket create logic

--- my-cdk-s3-bucket-2/lib/my-cdk-s3-bucket-2-stack.ts
+++ my-cdk-s3-bucket-2/lib/my-cdk-s3-bucket-2-stack.ts
@@ -1,19 +1,20 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
 
-export class MyCdkS3Bucket2Stack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class MyCdkS3Bucket2Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'MyCdkS3Bucket2Queue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'MyCdkS3Bucket2Topic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // Create a new S3 bucket
+    for (let i = 1; i <= 3; i++) {
+      console.log(`>>> Build S3 bucket :  ${i}`);
+      new s3.Bucket(this, `my-second-bucket-${i}`, {
+        bucketName: `my-second-cdk-bucket-${i}`, // Unique name for the bucket
+        versioned: true, // Enable versioning for the bucket
+        removalPolicy: cdk.RemovalPolicy.DESTROY, // Automatically delete the bucket when the stack is deleted
+        autoDeleteObjects: true, // Automatically delete objects when the bucket is deleted
+      });
+    }
   }
 }
