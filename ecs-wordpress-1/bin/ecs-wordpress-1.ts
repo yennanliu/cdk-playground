59b34662dc54aef6625afb507b61a43f55d53833
yenj12 <yenj12@uber.com>
2025-05-19 13:18:33 +0800

    wordpress stack fix stack contructor param

--- ecs-wordpress-1/bin/ecs-wordpress-1.ts
+++ ecs-wordpress-1/bin/ecs-wordpress-1.ts
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { EcsWordpress1Stack } from '../lib/ecs-wordpress-1-stack';
+import { EcsStack } from '../lib/ecs-stack';
+import { NetworkStack } from '../lib/network-stack';
 
 const app = new cdk.App();
-new EcsWordpress1Stack(app, 'EcsWordpress1Stack');
+
+// Create the network stack
+const networkStack = new NetworkStack(app, 'NetworkStack');
+
+// Pass the VPC from the network stack to the ECS stack
+new EcsStack(app, 'EcsWordpress1Stack', { vpc: networkStack.vpc });
--- ecs-wordpress-1/lib/ecs-wordpress-1-stack.ts
+++ ecs-wordpress-1/lib/ecs-wordpress-1-stack.ts
@@ -1,19 +1,19 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+// import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+// import * as sns from 'aws-cdk-lib/aws-sns';
+// import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+// import { Construct } from 'constructs';
 
-export class EcsWordpress1Stack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+// export class EcsWordpress1Stack extends Stack {
+//   constructor(scope: Construct, id: string, props?: StackProps) {
+//     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'EcsWordpress1Queue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
+//     const queue = new sqs.Queue(this, 'EcsWordpress1Queue', {
+//       visibilityTimeout: Duration.seconds(300)
+//     });
 
-    const topic = new sns.Topic(this, 'EcsWordpress1Topic');
+//     const topic = new sns.Topic(this, 'EcsWordpress1Topic');
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
-  }
-}
+//     topic.addSubscription(new subs.SqsSubscription(queue));
+//   }
+// }
