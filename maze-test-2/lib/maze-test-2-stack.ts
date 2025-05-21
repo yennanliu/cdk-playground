a792f5c5863e04042a64af2479ec078e52131751
yennanliu <f339339@gmail.com>
2025-05-21 20:27:20 +0800

    maze 2 implement stack, update dep

--- maze-test-2/lib/maze-test-2-stack.ts
+++ maze-test-2/lib/maze-test-2-stack.ts
@@ -1,19 +1,41 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps, RemovalPolicy, CfnOutput } from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
+import * as path from 'path';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class MazeTest2Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'MazeTest2Queue', {
-      visibilityTimeout: Duration.seconds(300)
+    // Create an S3 bucket for website hosting
+    const websiteBucket = new s3.Bucket(this, 'MazeWebsiteBucket', {
+      websiteIndexDocument: 'index.html',
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+      removalPolicy: RemovalPolicy.DESTROY, // For development purposes
+      autoDeleteObjects: true, // For development purposes
     });
 
-    const topic = new sns.Topic(this, 'MazeTest2Topic');
+    // Add bucket policy to allow public read access
+    websiteBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ['s3:GetObject'],
+        resources: [websiteBucket.arnForObjects('*')],
+        principals: [new iam.AnyPrincipal()],
+      })
+    );
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // Deploy the website files to the S3 bucket
+    new s3deploy.BucketDeployment(this, 'DeployMazeWebsite', {
+      sources: [s3deploy.Source.asset(path.join(__dirname, '../maze'))],
+      destinationBucket: websiteBucket,
+    });
+
+    // Output the website URL
+    new CfnOutput(this, 'WebsiteURL', {
+      value: websiteBucket.bucketWebsiteUrl,
+      description: 'The URL of the website',
+    });
   }
 }
