8f3dc51c4fdcd5c47c88e3a1a14ee17970423d7b
yenj12 <yenj12@uber.com>
2025-05-21 18:28:22 +0800

    update

--- my-cdk-s3-bucket-2/lib/my-cdk-s3-bucket-2-stack.ts
+++ my-cdk-s3-bucket-2/lib/my-cdk-s3-bucket-2-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class MyCdkS3Bucket2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -16,5 +17,39 @@ export class MyCdkS3Bucket2Stack extends cdk.Stack {
         autoDeleteObjects: true, // Automatically delete objects when the bucket is deleted
       });
     }
+
+    // create vpc
+    // const vpc = new ec2.Vpc(this, "MyVpc", {
+    //   maxAzs: 2, // Default is all AZs in the region
+    //   natGateways: 1});
+    const vpc = new ec2.Vpc(this, "MyVpc", {
+      maxAzs: 2, // Default is all AZs in the region
+    });
+
+    // creare security group
+    const securityGroup = new ec2.SecurityGroup(this, "MySecurityGroup", {
+      vpc,
+      allowAllOutbound: true, // Allow all outbound traffic
+    });
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(), // Allow all inbound traffic from any IPv4 address
+      ec2.Port.tcp(22), // Allow ssh access
+      "Allow HTTP traffic"
+    );
+
+    // add ec2
+    const instance = new ec2.Instance(this, "MyInstance", {
+      vpc,
+      instanceType: new ec2.InstanceType("t2.micro"),
+      machineImage: new ec2.AmazonLinuxImage(), // Use the latest Amazon Linux AMI
+      securityGroup: securityGroup,
+    });
+    // instance.userData.addCommands(
+    //   "yum update -y",
+    //   "yum install -y httpd",);
+
+
   }
+
+
 }
