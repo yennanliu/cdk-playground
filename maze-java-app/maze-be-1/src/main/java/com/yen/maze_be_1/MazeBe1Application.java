3f08baf8a754df224bbc193019f380d162a653b8
yennanliu <f339339@gmail.com>
2025-05-23 00:22:06 +0800

    be app add logger

--- maze-java-app/maze-be-1/src/main/java/com/yen/maze_be_1/MazeBe1Application.java
+++ maze-java-app/maze-be-1/src/main/java/com/yen/maze_be_1/MazeBe1Application.java
@@ -1,14 +1,19 @@
 package com.yen.maze_be_1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MazeBe1Application {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MazeBe1Application.class);
 
 	public static void main(String[] args) {
-
+		logger.info("Starting Maze Application");
 		SpringApplication.run(MazeBe1Application.class, args);
+		logger.info("Maze Application started successfully");
 	}
 
 }
