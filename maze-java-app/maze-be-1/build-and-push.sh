2113a3037a6d421a43e52d25d848d700c42b197c
yennanliu <f339339@gmail.com>
2025-05-23 00:17:18 +0800

    fix deploy script, dockerfile, build setting in mvn, readme

--- maze-java-app/maze-be-1/build-and-push.sh
+++ maze-java-app/maze-be-1/build-and-push.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+set -e
+
+# Set Java 17 as the Java version to use
+export JAVA_HOME="/Users/yennanliu/Library/Java/JavaVirtualMachines/corretto-17.0.8/Contents/Home"
+export PATH="$JAVA_HOME/bin:$PATH"
+
+echo "Using Java version:"
+java -version
 
 # Build the Spring Boot application
-# ./mvnw clean package -DskipTests
+./mvnw clean package -DskipTests
 
-# Build and push multi-architecture Docker image
-docker buildx create --use
-docker buildx build --platform linux/amd64,linux/arm64 -t yennanliu/maze-app:latest --push . 
+# Build the Docker image for AMD64 platform (standard EC2 instances)
+docker buildx build --platform=linux/amd64 -t yennanliu/maze-app:latest .
+
+# Push the image to Docker Hub
+docker push yennanliu/maze-app:latest
+
+echo "Image built and pushed successfully!" 
