23e5f46f29f50e5a3dc276263c23e257e33386d6
yenj12 <yenj12@uber.com>
2025-05-22 14:20:25 +0800

    add maze

---
+++ maze-java-app/maze-be-1/src/main/java/com/yen/maze_be_1/MazeBe1Application.java
@@ -0,0 +1,13 @@
+package com.yen.maze_be_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazeBe1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MazeBe1Application.class, args);
+	}
+
+}
