61293f9fc466b13a148affdeda173f6682beb641
yenj12 <yenj12@uber.com>
2025-06-03 17:16:18 +0800

    remove no need stack

--- api-gateway-lambda-token-authorizer-1/bin/api-gateway-lambda-token-authorizer-1.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { ApiGatewayLambdaTokenAuthorizer1Stack } from '../lib/api-gateway-lambda-token-authorizer-1-stack';
-
-const app = new cdk.App();
-new ApiGatewayLambdaTokenAuthorizer1Stack(app, 'ApiGatewayLambdaTokenAuthorizer1Stack');
--- auth-service/bin/auth-service.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { AuthServiceStack } from '../lib/auth-service-stack';
-
-const app = new cdk.App();
-new AuthServiceStack(app, 'AuthServiceStack');
