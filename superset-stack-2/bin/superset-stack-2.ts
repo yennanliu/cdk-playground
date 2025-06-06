448f1426a53e2393d2e6c3eb0440c0702d728c2b
yennanliu <f339339@gmail.com>
2025-06-06 20:18:56 +0800

    init 	superset-stack-2/

---
+++ superset-stack-2/bin/superset-stack-2.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { SupersetStack2Stack } from '../lib/superset-stack-2-stack';
+
+const app = new cdk.App();
+new SupersetStack2Stack(app, 'SupersetStack2Stack');
