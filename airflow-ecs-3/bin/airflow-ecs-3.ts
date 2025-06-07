270eac5f10c7f2bc2b4aa112c97061bdaef5a486
yennanliu <f339339@gmail.com>
2025-06-07 15:31:39 +0800

    init airflow-ecs-3/

---
+++ airflow-ecs-3/bin/airflow-ecs-3.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { AirflowEcs3Stack } from '../lib/airflow-ecs-3-stack';
+
+const app = new cdk.App();
+new AirflowEcs3Stack(app, 'AirflowEcs3Stack');
