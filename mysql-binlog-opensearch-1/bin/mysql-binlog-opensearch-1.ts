66dabe88c911a293baabb38e16e4085eb4072dfd
yennanliu <f339339@gmail.com>
2025-06-11 19:49:52 +0800

    init mysql-binlog-opensearch-1/ stack

---
+++ mysql-binlog-opensearch-1/bin/mysql-binlog-opensearch-1.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { MysqlBinlogOpensearch1Stack } from '../lib/mysql-binlog-opensearch-1-stack';
+
+const app = new cdk.App();
+new MysqlBinlogOpensearch1Stack(app, 'MysqlBinlogOpensearch1Stack');
