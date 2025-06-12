690db119271b569108032994a31c629a2eff5dc6
yenj12 <yenj12@uber.com>
2025-06-12 15:07:44 +0800

    fix az, instance type

--- cloudwatch-opensearch-1/lib/opensearch-stack.ts
+++ cloudwatch-opensearch-1/lib/opensearch-stack.ts
@@ -8,119 +8,120 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export interface OpensearchStackProps extends cdk.StackProps {
-  vpc: ec2.IVpc;
+    vpc: ec2.IVpc;
 }
 
 export class OpensearchStack extends cdk.Stack {
-  public readonly domain: opensearch.Domain;
-  public readonly firehoseRole: iam.Role;
+    public readonly domain: opensearch.Domain;
+    public readonly firehoseRole: iam.Role;
 
-  constructor(scope: Construct, id: string, props: OpensearchStackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props: OpensearchStackProps) {
+        super(scope, id, props);
 
-    // Create security group for OpenSearch
-    const opensearchSG = new ec2.SecurityGroup(this, 'OpenSearchSG', {
-      vpc: props.vpc,
-      description: 'Security group for OpenSearch domain',
-      allowAllOutbound: true,
-    });
-    opensearchSG.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(443),
-      'Allow HTTPS access from anywhere'
-    );
+        // Create security group for OpenSearch
+        const opensearchSG = new ec2.SecurityGroup(this, 'OpenSearchSG', {
+            vpc: props.vpc,
+            description: 'Security group for OpenSearch domain',
+            allowAllOutbound: true,
+        });
+        opensearchSG.addIngressRule(
+            ec2.Peer.anyIpv4(),
+            ec2.Port.tcp(443),
+            'Allow HTTPS access from anywhere'
+        );
 
-    // Create OpenSearch domain
-    this.domain = new opensearch.Domain(this, 'LogsDomain', {
-      version: opensearch.EngineVersion.OPENSEARCH_1_3,
-      vpc: props.vpc,
-      vpcSubnets: [{ subnetType: ec2.SubnetType.PUBLIC }],
-      securityGroups: [opensearchSG],
-      capacity: {
-        dataNodes: 1,
-        dataNodeInstanceType: 'r6g.large.search', // Using r6g instance type which better supports OpenSearch
-      },
-      ebs: {
-        volumeSize: 10,
-      },
-      zoneAwareness: {
-        enabled: false,
-      },
-      enforceHttps: true,
-      nodeToNodeEncryption: true,
-      encryptionAtRest: {
-        enabled: true,
-      },
-      // For development/testing purposes only
-      accessPolicies: [
-        new iam.PolicyStatement({
-          actions: ['es:*'],
-          resources: ['*'],
-          principals: [new iam.AnyPrincipal()],
-        }),
-      ],
-    });
+        // Create OpenSearch domain
+        this.domain = new opensearch.Domain(this, 'LogsDomain', {
+            version: opensearch.EngineVersion.OPENSEARCH_1_3,
+            vpc: props.vpc,
+            vpcSubnets: [{ subnetType: ec2.SubnetType.PUBLIC }],
+            securityGroups: [opensearchSG],
+            capacity: {
+                dataNodes: 1,
+                dataNodeInstanceType: 'm6g.large.search', // Using m6g.large for better compatibility
+            },
+            ebs: {
+                volumeSize: 10,
+            },
+            // Single AZ deployment
+            zoneAwareness: {
+                enabled: false
+            },
+            enforceHttps: true,
+            nodeToNodeEncryption: true,
+            encryptionAtRest: {
+                enabled: true,
+            },
+            // For development/testing purposes only
+            accessPolicies: [
+                new iam.PolicyStatement({
+                    actions: ['es:*'],
+                    resources: ['*'],
+                    principals: [new iam.AnyPrincipal()],
+                }),
+            ],
+        });
 
-    // Create IAM role for Firehose
-    this.firehoseRole = new iam.Role(this, 'FirehoseRole', {
-      assumedBy: new iam.ServicePrincipal('firehose.amazonaws.com'),
-    });
+        // Create IAM role for Firehose
+        this.firehoseRole = new iam.Role(this, 'FirehoseRole', {
+            assumedBy: new iam.ServicePrincipal('firehose.amazonaws.com'),
+        });
 
-    // Add permissions to Firehose role
-    this.firehoseRole.addToPolicy(
-      new iam.PolicyStatement({
-        resources: [this.domain.domainArn, `${this.domain.domainArn}/*`],
-        actions: [
-          'es:DescribeElasticsearchDomain',
-          'es:DescribeElasticsearchDomains',
-          'es:DescribeElasticsearchDomainConfig',
-          'es:ESHttpPost',
-          'es:ESHttpPut',
-        ],
-      })
-    );
+        // Add permissions to Firehose role
+        this.firehoseRole.addToPolicy(
+            new iam.PolicyStatement({
+                resources: [this.domain.domainArn, `${this.domain.domainArn}/*`],
+                actions: [
+                    'es:DescribeElasticsearchDomain',
+                    'es:DescribeElasticsearchDomains',
+                    'es:DescribeElasticsearchDomainConfig',
+                    'es:ESHttpPost',
+                    'es:ESHttpPut',
+                ],
+            })
+        );
 
-    // Create S3 bucket for Firehose backup
-    const backupBucket = new s3.Bucket(this, 'FirehoseBackupBucket', {
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // For development/testing only
-      autoDeleteObjects: true, // For development/testing only
-    });
+        // Create S3 bucket for Firehose backup
+        const backupBucket = new s3.Bucket(this, 'FirehoseBackupBucket', {
+            removalPolicy: cdk.RemovalPolicy.DESTROY, // For development/testing only
+            autoDeleteObjects: true, // For development/testing only
+        });
 
-    // Add S3 permissions to Firehose role
-    this.firehoseRole.addToPolicy(
-      new iam.PolicyStatement({
-        resources: [backupBucket.bucketArn, `${backupBucket.bucketArn}/*`],
-        actions: [
-          's3:AbortMultipartUpload',
-          's3:GetBucketLocation',
-          's3:GetObject',
-          's3:ListBucket',
-          's3:ListBucketMultipartUploads',
-          's3:PutObject',
-        ],
-      })
-    );
+        // Add S3 permissions to Firehose role
+        this.firehoseRole.addToPolicy(
+            new iam.PolicyStatement({
+                resources: [backupBucket.bucketArn, `${backupBucket.bucketArn}/*`],
+                actions: [
+                    's3:AbortMultipartUpload',
+                    's3:GetBucketLocation',
+                    's3:GetObject',
+                    's3:ListBucket',
+                    's3:ListBucketMultipartUploads',
+                    's3:PutObject',
+                ],
+            })
+        );
 
-    // Create Firehose delivery stream
-    const firehoseDeliveryStream = new firehose.CfnDeliveryStream(this, 'LogsDeliveryStream', {
-      deliveryStreamType: 'DirectPut',
-      elasticsearchDestinationConfiguration: {
-        domainArn: this.domain.domainArn,
-        indexName: 'logs',
-        roleArn: this.firehoseRole.roleArn,
-        s3BackupMode: 'FailedDocumentsOnly',
-        s3Configuration: {
-          bucketArn: backupBucket.bucketArn,
-          roleArn: this.firehoseRole.roleArn,
-          bufferingHints: {
-            intervalInSeconds: 60,
-            sizeInMBs: 1,
-          },
-        },
-      },
-    });
+        // Create Firehose delivery stream
+        const firehoseDeliveryStream = new firehose.CfnDeliveryStream(this, 'LogsDeliveryStream', {
+            deliveryStreamType: 'DirectPut',
+            elasticsearchDestinationConfiguration: {
+                domainArn: this.domain.domainArn,
+                indexName: 'logs',
+                roleArn: this.firehoseRole.roleArn,
+                s3BackupMode: 'FailedDocumentsOnly',
+                s3Configuration: {
+                    bucketArn: backupBucket.bucketArn,
+                    roleArn: this.firehoseRole.roleArn,
+                    bufferingHints: {
+                        intervalInSeconds: 60,
+                        sizeInMBs: 1,
+                    },
+                },
+            },
+        });
 
-    // Add tags
-    cdk.Tags.of(this).add('Project', 'LogPipeline');
-  }
+        // Add tags
+        cdk.Tags.of(this).add('Project', 'LogPipeline');
+    }
 }
