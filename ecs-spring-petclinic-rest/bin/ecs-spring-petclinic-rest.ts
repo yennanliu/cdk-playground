d7a0c454dc499f8a8e10f978f4e8f2eb2e339cf1
yennanliu <f339339@gmail.com>
2025-06-13 17:44:29 +0800

    remove ecs-spring-petclinic-rest stck

--- ecs-spring-petclinic-rest/bin/ecs-spring-petclinic-rest.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { EcsSpringPetclinicRestStack } from '../lib/ecs-spring-petclinic-rest-stack';
-
-const app = new cdk.App();
-new EcsSpringPetclinicRestStack(app, 'EcsSpringPetclinicRestStack');
