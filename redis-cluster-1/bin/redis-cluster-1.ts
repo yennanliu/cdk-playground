277202bc870bcab507ce937c701bbed3b8d54ad5
yenj12 <yenj12@uber.com>
2025-06-18 15:18:43 +0800

    add redis cluster stack 1

---
+++ redis-cluster-1/bin/redis-cluster-1.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { RedisCluster1Stack } from '../lib/redis-cluster-1-stack';
+
+const app = new cdk.App();
+new RedisCluster1Stack(app, 'RedisCluster1Stack');
