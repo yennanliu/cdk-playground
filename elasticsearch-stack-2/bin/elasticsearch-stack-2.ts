daf66a19224e05882ebf51e16fef60412d054821
yennanliu <f339339@gmail.com>
2025-07-01 15:31:20 +0800

    remove es stack

--- elasticsearch-stack-2/bin/elasticsearch-stack-2.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { ElasticsearchStack2Stack } from '../lib/elasticsearch-stack-2-stack';
-
-const app = new cdk.App();
-new ElasticsearchStack2Stack(app, 'ElasticsearchStack2Stack');
