61f557433346d201321463dba7611e76b566d9cc
yennanliu <f339339@gmail.com>
2025-07-02 17:26:12 +0800

    add dms stack 1

---
+++ dms-s3-stack-1/bin/dms-s3-stack-1.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { DmsS3Stack1Stack } from '../lib/dms-s3-stack-1-stack';
+
+const app = new cdk.App();
+new DmsS3Stack1Stack(app, 'DmsS3Stack1Stack');
