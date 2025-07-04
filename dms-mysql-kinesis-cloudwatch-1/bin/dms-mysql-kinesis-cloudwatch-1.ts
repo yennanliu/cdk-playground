cd6de1c3536ca5306e9fcb2e8e588db4abb9ccce
yennanliu <f339339@gmail.com>
2025-07-04 18:06:37 +0800

    init dms-mysql-kinesis-cloudwatch-1/

---
+++ dms-mysql-kinesis-cloudwatch-1/bin/dms-mysql-kinesis-cloudwatch-1.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { DmsMysqlKinesisCloudwatch1Stack } from '../lib/dms-mysql-kinesis-cloudwatch-1-stack';
+
+const app = new cdk.App();
+new DmsMysqlKinesisCloudwatch1Stack(app, 'DmsMysqlKinesisCloudwatch1Stack');
