cf79b814ac44852fcabb35eb86b42ca9ab359363
yennanliu <f339339@gmail.com>
2025-07-06 15:03:23 +0800

    remove projects

--- mlflow-stack-1/bin/mlflow-stack-1.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { MlflowStack1Stack } from '../lib/mlflow-stack-1-stack';
-
-const app = new cdk.App();
-new MlflowStack1Stack(app, 'MlflowStack1Stack');
