bb6ba24b4de7fbdff3b14c0c1f5f882bd5bcb222
yennanliu <f339339@gmail.com>
2025-07-12 16:04:27 +0800

    clean

--- eks-stack-2/bin/eks-stack-2.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { EksStack2Stack } from '../lib/eks-stack-2-stack';
-
-const app = new cdk.App();
-new EksStack2Stack(app, 'EksStack2Stack');
