01147558874d5b263ff54140778e30b7467314a0
jim <jim@uber.com>
2025-07-17 14:47:35 +0800

    fix

--- j-postgre-redis-ecs/bin/j-postgre-redis-ecs.ts
+++ j-postgre-redis-ecs/bin/j-postgre-redis-ecs.ts
@@ -3,4 +3,9 @@ import * as cdk from 'aws-cdk-lib';
 import { JPostgreRedisEcsStack } from '../lib/j-postgre-redis-ecs-stack';
 
 const app = new cdk.App();
-new JPostgreRedisEcsStack(app, 'JPostgreRedisEcsStack');
+new JPostgreRedisEcsStack(app, 'JPostgreRedisEcsStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+});
