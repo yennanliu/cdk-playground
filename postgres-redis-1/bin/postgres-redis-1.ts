0908a13fc1f8cede23d55d578dbe35bf6aa138d4
jim <jim@uber.com>
2025-07-17 14:26:56 +0800

    add

--- postgres-redis-1/bin/postgres-redis-1.ts
+++ postgres-redis-1/bin/postgres-redis-1.ts
@@ -3,4 +3,4 @@ import * as cdk from 'aws-cdk-lib';
 import { PostgresRedis1Stack } from '../lib/postgres-redis-1-stack';
 
 const app = new cdk.App();
-new PostgresRedis1Stack(app, 'PostgresRedis1Stack');
+new PostgresRedis1Stack(app, 'PostgresRedis1Stack-2');
