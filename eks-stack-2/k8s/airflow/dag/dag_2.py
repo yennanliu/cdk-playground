c42630dd33919a0dc79e2d068d616f9e2f72da2c
yennanliu <f339339@gmail.com>
2025-07-27 16:30:08 +0800

    use git sync dag in airflow k8s, add dag

---
+++ eks-stack-2/k8s/airflow/dag/dag_2.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+def hello():
+    for i in range(5):
+        print("i = " + str(i))
+        print("Hello from Airflow 2 !!!")
+
+with DAG(
+    dag_id="hello_world_dag_2",
+    start_date=datetime(2023, 1, 1),
+    schedule_interval="@once",  # run once
+    catchup=False
+) as dag:
+    task = PythonOperator(
+        task_id="say_hello",
+        python_callable=hello
+    )
