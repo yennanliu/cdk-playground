b66385f9a51a41febec2e5b0608c1f5cdff61e1f
yennanliu <f339339@gmail.com>
2025-08-02 17:01:17 +0800

    remove no needed opensearch stacks

--- mysql-binlog-opensearch-1/bin/mysql-binlog-opensearch-1.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { MysqlBinlogOpensearch1Stack } from '../lib/mysql-binlog-opensearch-1-stack';
-
-const app = new cdk.App();
-new MysqlBinlogOpensearch1Stack(app, 'MysqlBinlogOpensearch1Stack-2');
--- opensearch-stack-1/bin/opensearch-stack-1.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { OpensearchStack1Stack } from '../lib/opensearch-stack-1-stack';
-
-const app = new cdk.App();
-new OpensearchStack1Stack(app, 'OpensearchStack1Stack');
