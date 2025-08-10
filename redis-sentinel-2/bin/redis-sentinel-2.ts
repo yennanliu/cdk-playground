075781c9eb08aa532be8639d3d06dce52b97d83d
yennanliu <f339339@gmail.com>
2025-08-10 15:20:31 +0800

    add redis-sentinel-2/

---
+++ redis-sentinel-2/bin/redis-sentinel-2.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { RedisSentinel2Stack } from '../lib/redis-sentinel-2-stack';
+
+const app = new cdk.App();
+new RedisSentinel2Stack(app, 'RedisSentinel2Stack');
