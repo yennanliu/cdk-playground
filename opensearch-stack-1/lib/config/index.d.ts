4747719c4c8608b57d83486c0bf44dfa60a4f306
jim <jim@uber.com>
2025-08-11 14:11:48 +0800

    remove all js, compiled ts in lib

--- opensearch-stack-1/lib/config/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Construct } from "constructs";
-import { StackConfiguration } from "./types";
-export { StackConfiguration, ParsedOpenSearchConfig, ServiceLogConfig, LogConfig } from "./types";
-export { ConfigValidator } from "./validator";
-export declare class ConfigManager {
-    static loadConfiguration(scope: Construct, stage: string): StackConfiguration;
-    private static getDefaultsForStage;
-}
--- opensearch-stack-1/lib/config/parser.d.ts
+++
@@ -1,7 +0,0 @@
-import { Construct } from "constructs";
-import { RawConfigDefaults, StackConfiguration } from "./types";
-export declare class ConfigParser {
-    static parse(scope: Construct, stage: string, defaults: RawConfigDefaults): StackConfiguration;
-    private static getContextForType;
-    private static parseServices;
-}
