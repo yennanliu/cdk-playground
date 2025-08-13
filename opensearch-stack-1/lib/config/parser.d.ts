791ba04d1de8854f2cec35125ab1a7dfb66a9727
yennj12 <f339339@gmail.com>
2025-08-13 15:31:06 +0800

    opensearch-dev-14-update-pod-log (#47)
    * update pod processor lambda
    
    * remove js, compiled ts
    
    * remove default cloudwatch log group in dev, prod json
    
    ---------
    
    Co-authored-by: jim <jim@uber.com>

--- opensearch-stack-1/lib/config/parser.d.ts
+++
@@ -1,6 +0,0 @@
-import { Construct } from "constructs";
-import { RawConfigDefaults, StackConfiguration } from "./types";
-export declare class ConfigParser {
-    static parse(scope: Construct, stage: string, defaults: RawConfigDefaults): StackConfiguration;
-    private static getContextForType;
-}
