1922b418d8863ed4fd2a6924cf31f3ee58f948a1
jim <jim@uber.com>
2025-09-21 16:50:04 +0800

    LLMChatBot-1 implment CDK lib, lambda, html

--- LLMChatBot-1/lib/llm_chat_bot-1-stack.ts
+++ LLMChatBot-1/lib/llm_chat_bot-1-stack.ts
@@ -1,19 +1,104 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Duration, Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class LlmChatBot1Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'LlmChatBot1Queue', {
-      visibilityTimeout: Duration.seconds(300)
+    // S3 Bucket for frontend
+    const frontendBucket = new s3.Bucket(this, 'FrontendBucket', {
+      bucketName: undefined, // Let AWS generate unique name
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'error.html',
+      publicReadAccess: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
     });
 
-    const topic = new sns.Topic(this, 'LlmChatBot1Topic');
+    // CloudFront Distribution
+    const distribution = new cloudfront.Distribution(this, 'Distribution', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(frontendBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: 'index.html',
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // DynamoDB Table for chat history
+    const chatTable = new dynamodb.Table(this, 'ChatTable', {
+      tableName: 'ChatHistory',
+      partitionKey: { name: 'sessionId', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'timestamp', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    // Lambda function for chat processing
+    const chatLambda = new lambda.Function(this, 'ChatLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda'),
+      timeout: Duration.seconds(30),
+      environment: {
+        CHAT_TABLE_NAME: chatTable.tableName,
+        BEDROCK_REGION: this.region,
+      },
+    });
+
+    // Grant Lambda permissions
+    chatTable.grantReadWriteData(chatLambda);
+
+    // Bedrock permissions
+    chatLambda.addToRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        'bedrock:InvokeModel',
+      ],
+      resources: ['*'],
+    }));
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, 'ChatApi', {
+      restApiName: 'LLM Chat API',
+      description: 'API for LLM chatbot',
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+        allowHeaders: ['Content-Type', 'X-Amz-Date', 'Authorization', 'X-Api-Key'],
+      },
+    });
+
+    const chatResource = api.root.addResource('chat');
+    chatResource.addMethod('POST', new apigateway.LambdaIntegration(chatLambda));
+
+    // Outputs
+    this.exportValue(distribution.distributionDomainName, {
+      name: 'CloudFrontUrl',
+    });
+
+    this.exportValue(api.url, {
+      name: 'ApiUrl',
+    });
+
+    this.exportValue(frontendBucket.bucketName, {
+      name: 'FrontendBucketName',
+    });
+
+    // Deploy frontend files to S3
+    new s3deploy.BucketDeployment(this, 'DeployFrontend', {
+      sources: [s3deploy.Source.asset('./frontend')],
+      destinationBucket: frontendBucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
   }
 }
