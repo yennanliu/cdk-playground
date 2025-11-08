9572cef9e4d16d405d808c9d7a3a71c36ba6a5c6
jim <jim@uber.com>
2025-11-08 18:37:13 +0800

    implement EKS stack for lib/eks-stack-3-hpa-stack.ts

--- eks-stack-3-hpa/lib/eks-stack-3-hpa-stack.ts
+++ eks-stack-3-hpa/lib/eks-stack-3-hpa-stack.ts
@@ -1,19 +1,56 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps, CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as eks from 'aws-cdk-lib/aws-eks';
 import { Construct } from 'constructs';
 
 export class EksStack3HpaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'EksStack3HpaQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // Create VPC with public and private subnets
+    const vpc = new ec2.Vpc(this, 'EksVpc', {
+      maxAzs: 2,
+      natGateways: 1,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        }
+      ],
     });
 
-    const topic = new sns.Topic(this, 'EksStack3HpaTopic');
+    // Create EKS Cluster
+    const cluster = new eks.Cluster(this, 'EksCluster', {
+      version: eks.KubernetesVersion.V1_31,
+      vpc,
+      vpcSubnets: [{ subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS }],
+      defaultCapacity: 0,
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // Add managed node group
+    cluster.addNodegroupCapacity('NodeGroup', {
+      instanceTypes: [new ec2.InstanceType('t3.medium')],
+      minSize: 1,
+      maxSize: 3,
+      desiredSize: 2,
+      diskSize: 20,
+    });
+
+    // Outputs
+    new CfnOutput(this, 'ClusterName', {
+      value: cluster.clusterName,
+      description: 'EKS Cluster Name',
+    });
+
+    new CfnOutput(this, 'ConfigCommand', {
+      value: `aws eks update-kubeconfig --region ${this.region} --name ${cluster.clusterName}`,
+      description: 'Command to configure kubectl',
+    });
   }
 }
