fd7dd4e7813144bfe710fc1a7714c3dd4c8ce39c
jim <jim@uber.com>
2025-11-29 17:39:35 +0800

    add sagemaker stack 1

---
+++ sagemaker-stack-1/bin/sagemaker-stack-1.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { SagemakerStack1Stack } from '../lib/sagemaker-stack-1-stack';
+
+const app = new cdk.App();
+new SagemakerStack1Stack(app, 'SagemakerStack1Stack');
