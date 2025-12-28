d2d5e7d4a3347b3725cb1f222035a47ef4081146
jim <jim@uber.com>
2025-12-28 17:46:26 +0800

    add stock-summary-stack-1/

---
+++ stock-summary-stack-1/bin/stock-summary-stack-1.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { StockSummaryStack1Stack } from '../lib/stock-summary-stack-1-stack';
+
+const app = new cdk.App();
+new StockSummaryStack1Stack(app, 'StockSummaryStack1Stack');
